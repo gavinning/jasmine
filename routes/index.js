b3265987f42e0b449b7855a9bc4c8e5ca87c6cd9
gavinning <ningyubo@gmail.com>
2016-01-15 20:06:34 +0800

    port 3000 => port 8000

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/static/sage');
+  // res.render('index', { title: 'Express' });
   // req.hostname === '127.0.0.1' ?
   //     res.redirect('/static/sage'):
   //     res.render('index', { title: 'Express' });
